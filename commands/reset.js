d8aadd765626dcf021df8fdcc2fc622b06863f11
transiSciences <transiSciences@gmail.com>
2018-10-20 13:30:53 +0200

    Update commands/reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -11,7 +11,6 @@ exports.run = (Client, message, args) => {
             economy.fetchBalance(usr.id).then(db => {
                 let targetToWithdraw = parseInt(db.money);
                 total_withdrawing = targetToWithdraw - (targetToWithdraw * 2);
-                console.log(total_withdrawing)
             })
             economy.updateBalance(usr.id, -392);
             economy.fetchBalance(usr.id).then(db => {
